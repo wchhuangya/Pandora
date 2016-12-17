4f6f9cde3c515580cb92af128cfec8b5a947ce00
wchhuangya <wchhuangyawork@163.com>
2016-12-17 21:04:40 +0800

    1. 优化：打开新闻详情时，1. 不显示加载动画的问题；2. 时间默认是1900-01-01 00:00:00的问题；

--- app/src/main/java/com/ch/wchhuangya/android/pandora/vm/news/NewsDetailVM.java
+++ app/src/main/java/com/ch/wchhuangya/android/pandora/vm/news/NewsDetailVM.java
@@ -14,6 +14,10 @@ import com.ch.wchhuangya.android.pandora.vm.BaseVM;
 import com.ch.wchhuangya.lib.util.Constant;
 import com.ch.wchhuangya.lib.util.StringUtil;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+
 /**
  * Created by wchya on 2016-12-12 17:13
  */
@@ -49,37 +53,39 @@ public class NewsDetailVM extends BaseVM {
 
     public void loadData(WebView webView) {
         toolbarTitle.set(mTitle);
-        mSubscriptions.add(NewsHandle.getNewsDetail(mNewsId, mTableNum,
-                newsDetail -> {
-                    type.set(mTableNum);
-                    NewsDetail.DataBean data = newsDetail.getData();
-                    newsTitle.set(data.getTitle());
-                    source.set(data.getSource());
-                    date.set(Long.parseLong(data.getEdit_time()));
-                    String html = "<div style='line-height: 2;'>" + (mTableNum == 6 ? data.getDigest() : data.getContent()) + "</div>";
-                    Log.d(Constant.LOG_TAG, "loadData: 网页内容——" + html);
-                    webView.loadDataWithBaseURL(null, StringUtil.adjustImgFitScreen(html), "text/html", "UTF-8", null);
-                    if (StringUtil.isNotEmpty(data.getTop_image())) {
-                        if (!imgUrls.contains(data.getTop_image()))
-                            imgUrls.add(data.getTop_image());
+        mSubscriptions.add(Observable.timer(1, TimeUnit.SECONDS).subscribe(aLong ->
+            mSubscriptions.add(NewsHandle.getNewsDetail(mNewsId, mTableNum,
+                    newsDetail -> {
+                        type.set(mTableNum);
+                        NewsDetail.DataBean data = newsDetail.getData();
+                        newsTitle.set(data.getTitle());
+                        source.set(data.getSource());
+                        date.set(Long.parseLong(data.getEdit_time()));
+                        String html = "<div style='line-height: 2;'>" + (mTableNum == 6 ? data.getDigest() : data.getContent()) + "</div>";
+                        Log.d(Constant.LOG_TAG, "loadData: 网页内容——" + html);
+                        webView.loadDataWithBaseURL(null, StringUtil.adjustImgFitScreen(html), "text/html", "UTF-8", null);
+                        if (StringUtil.isNotEmpty(data.getTop_image())) {
+                            if (!imgUrls.contains(data.getTop_image()))
+                                imgUrls.add(data.getTop_image());
+                        }
+                        if (StringUtil.isNotEmpty(data.getText_image0())) {
+                            if (!imgUrls.contains(data.getText_image0()))
+                                imgUrls.add(data.getText_image0());
+                        }
+                        if (StringUtil.isNotEmpty(data.getText_image1())) {
+                            if (!imgUrls.contains(data.getText_image1()))
+                                imgUrls.add(data.getText_image1());
+                        }
+                    },
+                    throwable -> {
+                        pbShow.set(false);
+                        Log.e(Constant.LOG_TAG, "loadData: 加载新闻详情失败！", throwable);
+                    },
+                    () -> {
+                        pbShow.set(false);
                     }
-                    if (StringUtil.isNotEmpty(data.getText_image0())) {
-                        if (!imgUrls.contains(data.getText_image0()))
-                            imgUrls.add(data.getText_image0());
-                    }
-                    if (StringUtil.isNotEmpty(data.getText_image1())) {
-                        if (!imgUrls.contains(data.getText_image1()))
-                            imgUrls.add(data.getText_image1());
-                    }
-                },
-                throwable -> {
-                    pbShow.set(false);
-                    Log.e(Constant.LOG_TAG, "loadData: 加载新闻详情失败！", throwable);
-                },
-                () -> {
-                    pbShow.set(false);
-                }
-        ));
+            )
+        )));
     }
 
     @Override
